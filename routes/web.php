d3c3649b1a946e1ff2702de48d493ab57f94be6a
Javad Jafari <146510618+JavadJafari80@users.noreply.github.com>
2024-12-14 12:25:25 +0330

    create migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
 });
