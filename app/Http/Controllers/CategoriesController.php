c2847d4463eb2291f43e54ed6c43c2408cedf1ba
Javad Jafari <146510618+JavadJafari80@users.noreply.github.com>
2024-12-24 14:52:15 +0330

    create categories

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -17,13 +17,13 @@ class CategoriesController extends Controller
 
     public function store(Request $request)
     {
-//        Categories::create([
-//            'PTitle' => $request->PTitle,
-//            'ETitle' => $request->ETitle,
-//            'user_id' => Auth::user()->id,
-//            'parent_id' => $request->parent_id == 0 ? null : $request->parent_id,
-//            'is_active' => true,
-//        ]);
+        Categories::create([
+            'PTitle' => $request->PTitle,
+            'ETitle' => $request->ETitle,
+            'user_id' => Auth::user()->id,
+            'parent_id' => $request->parent_id == 0 ? null : $request->parent_id,
+            'is_active' => true,
+        ]);
 
         return redirect()->back()->with('success', 'دسته‌بندی با موفقیت اضافه شد.');
     }
